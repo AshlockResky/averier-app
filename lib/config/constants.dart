94c4ad6029809c7b9286e0427c6bd704e2c78b43
AshlockResky <samuelsundayloko1@gmail.com>
2026-09-01 22:25:10 +0100

    Update constants.dart

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,5 +1,9 @@
 class AppConstants {
   static const double minWithdrawal = 50.0;
   static const int minViewsToEarn = 5000;
-  static const double creatorRevenueShare = 0.5;
+  static const double creatorRevenueShare = 0.5; // 50%
+
+  // How much ad revenue is generated when a video becomes eligible
+  // (you can change this number later)
+  static const double adRevenuePerEligibleVideo = 6.0; // $6 per eligible video
 }
